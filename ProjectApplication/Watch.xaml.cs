93276b1c31ea33e3864728274f6b0ab0e1afb9e0
Иван <ivanbesch@outlook.com>
2016-06-30 01:41:16 +0300

    s.s. watch is enable

--- ProjectApplication/Watch.xaml.cs
+++ ProjectApplication/Watch.xaml.cs
@@ -19,19 +19,109 @@ namespace ProjectApplication
     /// Логика взаимодействия для Calendar.xaml
     /// </summary>
     public partial class Watch : Window
-    { 
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int counter;
         public Watch()
         {
             InitializeComponent();
-            startclock();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(TimerTick);
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+
+            switch(counter)
+            {
+                case 0:
+                    sec_a_.Fill = Brushes.Yellow;
+                    sec_b_.Fill = Brushes.Yellow;
+                    sec_c_.Fill = Brushes.Yellow;
+                    sec_e_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    sec_g_.Fill = Brushes.Yellow;
+                    break;
+                case 1:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_g_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    break;
+                case 2:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_c_.Fill = Brushes.Yellow;
+                    sec_g_.Fill = Brushes.Yellow;
+                    sec_d_.Fill = Brushes.Yellow;
+                    sec_a_.Fill = Brushes.Yellow;
+                    sec_e_.Fill = Brushes.Yellow;
+                    break;
+                case 3:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_c_.Fill = Brushes.Yellow;
+                    sec_g_.Fill = Brushes.Yellow;
+                    sec_d_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    sec_e_.Fill = Brushes.Yellow;
+                    break;
+                case 4:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_b_.Fill = Brushes.Yellow;
+                    sec_g_.Fill = Brushes.Yellow;
+                    sec_d_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    break;
+                case 5:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_c_.Fill = Brushes.Yellow;
+                    sec_b_.Fill = Brushes.Yellow;
+                    sec_d_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    sec_e_.Fill = Brushes.Yellow;
+                    break;
+                case 6:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_c_.Fill = Brushes.Yellow;
+                    sec_b_.Fill = Brushes.Yellow;
+                    sec_d_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    sec_e_.Fill = Brushes.Yellow;
+                    sec_a_.Fill = Brushes.Yellow;
+                    break;
+                case 7:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_c_.Fill = Brushes.Yellow;
+                    sec_g_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    break;
+                case 8:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_c_.Fill = Brushes.Yellow;
+                    sec_b_.Fill = Brushes.Yellow;
+                    sec_d_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    sec_e_.Fill = Brushes.Yellow;
+                    sec_g_.Fill = Brushes.Yellow;
+                    sec_a_.Fill = Brushes.Yellow;
+                    break;
+                case 9:
+                    sec_a_.Fill = sec_b_.Fill = sec_c_.Fill = sec_d_.Fill = sec_e_.Fill = sec_f_.Fill = sec_g_.Fill = Brushes.White;
+                    sec_c_.Fill = Brushes.Yellow;
+                    sec_b_.Fill = Brushes.Yellow;
+                    sec_d_.Fill = Brushes.Yellow;
+                    sec_f_.Fill = Brushes.Yellow;
+                    sec_g_.Fill = Brushes.Yellow;
+                    break;
+            }
+            counter++;
         }
 
         private void startclock()
         {
-            DispatcherTimer timer = new DispatcherTimer();
+           /* DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += tickevent;
-            timer.Start();
+            timer.Start(); */
         }
 
         private void tickevent(object sender, EventArgs e)
